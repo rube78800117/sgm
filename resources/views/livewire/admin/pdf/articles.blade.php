ae080faea8bd1706ab301fb4ef6906c2da88c970
ruben <rube78800117@gmail.com>
2024-06-26 18:08:02 -0400

    inicio construccion vista de pdf

--- resources/views/livewire/admin/pdf/articles.blade.php
+++ resources/views/livewire/admin/pdf/articles.blade.php
@@ -1,21 +1,34 @@
-<div>
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" href="assets/vendor/fonts/remixicon/remixicon.css" />
+        <title>Titulo</title>
+       <link rel="stylesheet" href="{{ public_path("assets/sass/report-print.scss") }}" media="all"/> 
 
+       
+    </head>
+   
+<body>
+    
 
+<table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Código QR</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($articles as $item)
+                <tr>
+                    <td>{{ $item->name }}</td>
+                    <td>
+                        <img src="data:image/png;base64, {{ DNS2D::getBarcodePNG(url('/articulo/' . $item->id), 'QRCODE') }}" alt="barcode" class="barcode">
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
-
-    @foreach ($articles as $item)
-        <div class="flex">
-            <span class="qrcode" style="transform: scale(0.3);">{!! DNS2D::getBarcodeHTML(url('/articulo/' . $item->id), 'QRCODE') !!}
-
-            </span>
-
-
-            <div>
-                {{ $item->name }}
-            </div>
-        </div>
-    @endforeach
-
-
-
-</div>
+</body>
